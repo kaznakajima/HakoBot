fa4618d2d77735d62d7684f2ea9754d939abbc16
Yukunon <yukunon.gamer@gmail.com>
2018-09-28 14:52:44 +0900

    イベントUI用のScriptを追加（未完成）

---
+++ HakoBotProj/Assets/Osada/Script/EventUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using UniRx;
+using UniRx.Triggers;
+
+public class EventUI : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+}
