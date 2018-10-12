23257e24250ab59d8958929d45e520ea95a9f823
Yukunon <yukunon.gamer@gmail.com>
2018-10-12 14:59:04 +0900

    ポイボットのAI　移動部分のβ版は完了　現在荷物投げの部分に取り掛かり中

--- HakoBotProj/Assets/Osada/Script/AI_Poibot.cs
+++ HakoBotProj/Assets/Osada/Script/AI_Poibot.cs
@@ -11,47 +11,90 @@ public class AI_Poibot : MonoBehaviour
     //移動方向
     public enum Direction
     {
-        vertical,
-        Horizontal
+        vertical,   //縦移動
+        Horizontal  //横移動
     }
     public Direction direction;
-
     //移動範囲
     private float moveRange;
-
+    //移動速度
     private float speed;
+    //移動中かの判断
+    private BoolReactiveProperty actionEnd = new BoolReactiveProperty(false);
 
     //荷物プレハブ
     private GameObject baggagePre;
 
+    //移動開始位置と目標位置
+    private Vector3 startPos, endPos;
+    //移動開始時間保存用
+    private float startTime = 0.0f;
+
     private void Start()
     {
-        this.UpdateAsObservable()
+        //移動開始
+        actionEnd.Where(c => !c)
+            .Subscribe(c =>
+            {
+                //開始位置と目標位置を設定
+                startPos = transform.position;
+                endPos = SearchMoveTarget();
+                //開始時間を保存
+                startTime = Time.timeSinceLevelLoad;
+
+                StartCoroutine("Move");
+            }).AddTo(this);
+
+        //荷物投げ開始
+        actionEnd.Where(c => c)
             .Subscribe(c =>
             {
 
             }).AddTo(this);
+    }
 
+    //移動目標地点検索
+    private Vector3 SearchMoveTarget()
+    {
+        var t = Random.Range(-moveRange, moveRange);
+        Vector3 pos = transform.position;
+        switch (direction)
+        {
+            case Direction.Horizontal:
+                pos.x = t;
+                break;
+            case Direction.vertical:
+                pos.z = t;
+                break;
+        }
+        return pos;
+    }
 
-        var time = Random.Range(3, 5);
-        //ランダムの時間で荷物を投げさせる
-        Observable.Timer(System.TimeSpan.FromSeconds(time)).
-            Subscribe(c =>
+    //移動処理
+    private IEnumerator Move()
+    {
+        //投げるまでの時間を設定
+        var time = Random.Range(3.0f, 5.0f);
+
+        while (true)
+        {
+            //現在の経過時間
+            var diff = Time.timeSinceLevelLoad - startTime;
+            //割合計算
+            var rate = diff / time;
+
+            if (rate <= 1)
             {
-
-                time = Random.Range(3, 5);
-            }).AddTo(this);
-    }
-
-    private void Move()
-    {
-        //前進
-        transform.position += transform.forward * speed * Time.deltaTime;
-    }
-
-    private void Turn()
-    {
-        //反転した時の角度
-        var dir = Quaternion.Euler(0, transform.rotation.y + 90, 0);
+                //移動処理
+                transform.position = Vector3.Lerp(startPos, endPos, rate);
+                yield return null;
+            }
+            else
+            {
+                //移動完了、荷物投げに移行
+                actionEnd.Value = true;
+                break;
+            }
+        }
     }
 }
