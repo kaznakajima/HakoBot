6d318cb19929f6b4224a233f9ee7a6ce66374d81
nkc.kazuki.nakashima <nkc.kazuki.nakashima@gmail.com>
2018-10-18 13:38:24 +0900

    ポイントの途中経過を表示するテキスト作成

---
+++ HakoBotProj/Assets/ShimaKun/Effect/RankText/RankText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankText : MonoBehaviour
+{
+    public TryScript tryScript;
+    // Use this for initialization
+    void Start ()
+    {
+        int RankText;
+        //         スクリプト名、変数名
+        RankText = tryScript.text;
+        this.GetComponent<Text>().text = "スコア"+ RankText.ToString()+"点";
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+}
