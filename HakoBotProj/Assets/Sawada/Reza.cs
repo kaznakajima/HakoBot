a78e88546e78e4f71e9581dbaa0b099d1d37525d
DESKTOP-28F38VP\user <nkc.syougo.sawada@cmail.com>
2018-10-22 14:53:57 +0900

    レーザ・スッポトライト

---
+++ HakoBotProj/Assets/Sawada/Reza.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reza : MonoBehaviour {
+    void OnParticleCollision(GameObject obj)
+    {
+        Debug.Log("衝突");
+        //Destroy(obj);
+        
+    }
+    // Use this for initialization
+    void Start () {
+      
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("Reza");
+    }
+}
---
+++ HakoBotProj/Assets/Sawada/light.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light : MonoBehaviour {
+    public Light spotlight;
+    public GameObject rbot;
+    Vector3 vector = Vector3.zero;
+    float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+       
+      
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 spotlitpostion = spotlight.transform.position;
+        Vector3 rbotpotion = rbot.transform.position;
+        Vector3 mokutekipostion= new Vector3(rbotpotion.x, spotlitpostion.y, rbotpotion.z);
+        spotlight.transform.position = Vector3.SmoothDamp(spotlitpostion, mokutekipostion, ref vector, speed);
+        //spotlight.transform.position = new Vector3(rbotpotion.x, spotlitpostion.y, rbotpotion.z);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (spotlight.enabled == true)
+            {
+                spotlight.enabled = false;
+            }
+           else if (spotlight.enabled == false)
+            {
+                spotlight.enabled = true;
+            }
+        }
+	}
+}
