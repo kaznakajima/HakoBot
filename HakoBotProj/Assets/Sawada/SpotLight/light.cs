739af694ca07f8c02e05ef0e40121051f85348d1
DESKTOP-28F38VP\user <nkc.syougo.sawada@cmail.com>
2018-10-24 14:50:55 +0900

    スッポトライトの変更

--- HakoBotProj/Assets/Sawada/SpotLight/light.cs
+++ HakoBotProj/Assets/Sawada/SpotLight/light.cs
@@ -3,24 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class light : MonoBehaviour {
-    public Light spotlight;
+   
     public GameObject rbot;
-    Vector3 vector = Vector3.zero;
-    float speed = 0.5f;
+    public Light spotlight;
 	// Use this for initialization
 	void Start () {
-       
-      
+
+    
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 spotlitpostion = spotlight.transform.position;
-        Vector3 rbotpotion = rbot.transform.position;
-        Vector3 mokutekipostion= new Vector3(rbotpotion.x, spotlitpostion.y, rbotpotion.z);
-        spotlight.transform.position = Vector3.SmoothDamp(spotlitpostion, mokutekipostion, ref vector, speed);
-        //spotlight.transform.position = new Vector3(rbotpotion.x, spotlitpostion.y, rbotpotion.z);
-
+        var aim =rbot.transform.position - spotlight.transform.position;
+        var look = Quaternion.LookRotation(aim);
+        spotlight.transform.localRotation = look;
         if (Input.GetKeyDown(KeyCode.Z))
         {
             if (spotlight.enabled == true)
