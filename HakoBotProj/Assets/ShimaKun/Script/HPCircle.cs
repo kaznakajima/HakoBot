c3c8396014d33f11c600928a4e27548e61b00a93
nkc.kazuki.nakashima <nkc.kazuki.nakashima@gmail.com>
2018-11-22 10:32:05 +0900

    HPバーを減らせるようにしてもらった。

---
+++ HakoBotProj/Assets/ShimaKun/Script/HPCircle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPCircle : MonoBehaviour
+{
+    GameObject iamge;
+	// Use this for initialization
+	void Start () {
+        iamge = GameObject.Find("HPCircle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void HPDown(float current,int max)
+    {
+        iamge.GetComponent<Image>().fillAmount = current / max;
+        
+    }
+}
---
+++ HakoBotProj/Assets/ShimaKun/Script/HPDamage.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+
+public class HPDamage : MonoBehaviour
+{
+    //最大HP
+    [SerializeField]
+    int maxHP = 100;
+    //現在のHP
+    [SerializeField]
+    float currentHP;
+
+    //GameObject textObj;
+    Text text;
+    GameObject hpSystem;
+
+   public float interval = 0;
+
+    bool flg = false;
+
+    private static readonly int PollingInterval = 60 * 1000;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //TextをGameObjectとして取得する
+        //textObj = GameObject.Find("HPDamage");//
+        //HPSystemを取得する
+        hpSystem = GameObject.Find("HPCircle");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //TextのTextコンポーネントにアクセス
+        //(int)はfloatを整数で表示するためのもの
+        //textObj.GetComponent<Text>().text = ((int)currentHP).ToString();
+        //textObj.GetComponent<RankText>().test((int)currentHP);
+
+        //HPSystemのスクリプトのHPDown関数に2つの数値を送る
+        hpSystem.GetComponent<HPCircle>().HPDown(currentHP, maxHP);
+
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            flg = true;
+            //currentHPが0以上ならTrue
+            //if (0 <= currentHP || currentHP <= 100)
+            //{
+
+            //    maxHPから秒数（×10）を引いた数がcurrentHP
+            //    currentHP += 10.0f;
+            //}
+            if(flg)
+            StartCoroutine(Interval());
+               
+        }
+
+            
+            
+            
+            
+    }
+
+    //void Shimakun()
+    //   {
+    //       for (int i = 0; i < 10; i++)
+    //       {
+    //           interval += Time.deltaTime;
+    //           if (interval >= 1.0f)
+    //           {
+    //               interval = 0;
+    //               //  currentHPが0以上ならTrue
+    //               
+    //           }
+
+    //       }flg = false;
+    //   }
+
+    private IEnumerator Interval()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (0 <= currentHP || currentHP <= 100)
+            {
+
+                // maxHPから秒数（×10）を引いた数がcurrentHP
+                currentHP += 1.0f;
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+        flg = false;
+    }
+}
