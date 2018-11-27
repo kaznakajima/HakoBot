b7e6f054ecda1ce175031b7fcbd4e7120c1bf8fb
Yukunon <yukunon.gamer@gmail.com>
2018-11-27 15:01:57 +0900

    タイトルシーンの改良

--- HakoBotProj/Assets/Osada/Script/TitleSystem.cs
+++ HakoBotProj/Assets/Osada/Script/TitleSystem.cs
@@ -1,8 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using UniRx;
+using UniRx.Triggers;
 
 public class TitleSystem : MonoBehaviour
 {
+    public PlayableDirector m_TiteTimeline;
+    public PlayableDirector m_StartTimeline;
 
+    private void Start()
+    {
+        this.UpdateAsObservable().Subscribe(_ =>
+        {
+            if (m_TiteTimeline.state != PlayState.Playing && Input.GetMouseButtonDown(0))
+            {
+                m_StartTimeline.Play();
+            }
+            else if (Input.GetMouseButtonDown(0))
+            {
+                m_TiteTimeline.time = 12.5f;
+                m_StartTimeline.Play();
+            }
+        }).AddTo(this);
+    }
 }
