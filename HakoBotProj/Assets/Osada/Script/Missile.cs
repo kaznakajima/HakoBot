0f5a8533cdfa53bbfd64ab09eb15598d650b5ec4
加速の妖精 <kasokunoyousei@gmail.com>
2018-12-08 22:42:03 +0900

    ミサイルScript作成 ミサイル発射準備Scriptは未作成です

--- HakoBotProj/Assets/Osada/Script/Missile.cs
+++ HakoBotProj/Assets/Osada/Script/Missile.cs
@@ -7,32 +7,36 @@ using UniRx.Triggers;
 public class Missile : MonoBehaviour
 {
     [SerializeField]
-    private GameObject effectObj;
-    [SerializeField]
     private Vector3 m_TargetPos;
-    private float speed = 1.0f;
+
+    private Vector3 m_Velocity = Vector3.zero;
+    private float m_Speed = 6.0f;
+
+    [SerializeField]
+    private GameObject m_ExplosionObj;
 
     private void Start()
     {
-        this.UpdateAsObservable().Subscribe(_ =>
-        {
-            if (Vector3.Distance(transform.position, m_TargetPos) <= 0.4f)
+        this.UpdateAsObservable()
+            .Subscribe(_ =>
             {
-                transform.position = m_TargetPos;
-                //爆発エフェクトを出す
-                effectObj.SetActive(true);
+                var dir = (m_TargetPos - transform.position).normalized;
+                var dis = Vector3.Distance(transform.position, m_TargetPos);
+                var currentVelocity = m_Velocity;
+                m_Velocity = dis > 0.3f ? dir * m_Speed : Vector3.zero;
+                m_Velocity = Vector3.Lerp(currentVelocity, m_Velocity, Mathf.Min(Time.deltaTime + 5.0f, 1));
 
-                Observable.Timer(System.TimeSpan.FromSeconds(0.5f))
-                .Subscribe(__ =>
+
+                Quaternion characterTargetRotation = Quaternion.LookRotation(dir);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, characterTargetRotation, 360.0f * Time.deltaTime);
+                transform.position += m_Velocity * Time.deltaTime;
+
+                if (dis < 0.3f)
                 {
-                    Destroy(gameObject);
-                }).AddTo(this);
-            }
-            else
-            {
-                transform.position = Vector3.Lerp(transform.position, m_TargetPos, speed * Time.deltaTime);
-                transform.localRotation = Quaternion.LookRotation((m_TargetPos - transform.position),Vector3.up);
-            }
-        }).AddTo(this);
+                    m_ExplosionObj.SetActive(true);
+
+                    Observable.Timer(System.TimeSpan.FromSeconds(0.5f)).Subscribe(__ => Destroy(gameObject)).AddTo(this);
+                }
+            }).AddTo(this);
     }
 }
--- HakoBotProj/Assets/Osada/Script/Sample.cs
+++ HakoBotProj/Assets/Osada/Script/Sample.cs
@@ -6,24 +6,11 @@ using UniRx.Triggers;
 
 public class Sample : MonoBehaviour
 {
-    [SerializeField]
-    private Vector3 pos;
+    //ミサイル発射準備Script
 
-    private Vector3 dir = Vector3.zero;
-    private Vector3 vec = Vector3.zero;
+    
+
     private void Start()
     {
-        this.UpdateAsObservable()
-            .Subscribe(_ =>
-            {
-                transform.rotation = Quaternion.LookRotation(pos - transform.position);
-
-                vec = dir;
-                dir = (pos - transform.position).normalized;
-                dir = Vector3.Lerp(vec, dir, Time.deltaTime);
-                Debug.Log(dir);
-
-                transform.position = dir * Time.deltaTime;
-            }).AddTo(this);
     }
 }
