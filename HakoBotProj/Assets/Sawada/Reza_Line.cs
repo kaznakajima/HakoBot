1e41d869cc585b00cf3b8f22e93a04f9f8e7a382
DESKTOP-28F38VP\user <nkc.syougo.sawada@cmail.com>
2019-01-16 13:42:11 +0900

    バックアップ

---
+++ HakoBotProj/Assets/Sawada/Reza_Line.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reza_Line : MonoBehaviour {
+    public Transform cube1;
+    public Transform cube2;
+    public Transform cube3;
+    // Use this for initialization
+    void Start () {
+       
+	}
+
+    // Update is called once per frame
+    void Update() {
+        LineRenderer render = GetComponent<LineRenderer>();
+        render.positionCount = 4;
+        render.SetPosition(0, new Vector3(4, 0, 0));
+        render.SetPosition(1, cube1.position);
+        render.SetPosition(2, cube2.position);
+        render.SetPosition(3, cube3.position);
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            render.enabled = true;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            render.enabled = false;
+        }
+    }
+}
---
+++ HakoBotProj/Assets/Sawada/denki.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class denki : MonoBehaviour {
+
+    // Use this for initialization
+
+    [SerializeField]
+    Transform[] posList;
+
+    [SerializeField]
+    GameObject obj;
+    private void Start()
+    {
+        foreach (Transform pos in posList)
+        {
+            obj.transform.position = pos.position;
+        }
+    }
+}
