00f69065260139a0be94e704322f3578bdd094a8
加速の妖精 <kasokunoyousei@gmail.com>
2019-01-20 22:52:32 +0900

    Script一部改良

---
+++ HakoBotProj/Assets/Osada/Script/Event/EventMessage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventMessage : MonoBehaviour
+{
+    [SerializeField]
+    private EventMessageData m_MessageData; 
+
+    public void SendMessege()
+    {
+
+    }
+}
