e3e0d5163ef5caae08616a7d4c905ec902d96a83
nkc.kazuki.nakashima <nkc.kazuki.nakashima@gmail.com>
2019-01-22 13:39:03 +0900

    テキストを移動させる

--- HakoBotProj/Assets/ShimaKun/Script/test.cs
+++ HakoBotProj/Assets/ShimaKun/Script/test.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class test : MonoBehaviour
 {
-    public float size = -1.0f;
-    
+    public Text text;
+
+    public GameObject CanvasPos;
 
     // Use this for initialization
     void Start () {
@@ -15,11 +16,57 @@ public class test : MonoBehaviour
         text.rectTransform.sizeDelta = new Vector2(text.preferredWidth, text.preferredHeight);
         //2回目でHeigjhtの高さが合わさる
         text.rectTransform.sizeDelta = new Vector2(text.preferredWidth, text.preferredHeight);
+        StartCoroutine("TextMove");
+        
     }
 
     // Update is called once per frame
     void Update () {
+    }
+
+    void Test(string str)
+    {
+        text.text = str;    
+    }
+
+    //必要な処理　実行前スタート位置に戻す　サイズに応じてスタート位置を変化させること　キャンバスのサイズを取得 
+    public IEnumerator TextMove()
+    {
+        //textサイズ取得 width height
+        float width = text.gameObject.GetComponent<RectTransform>().sizeDelta.x;
+        float height = text.gameObject.GetComponent<RectTransform>().sizeDelta.y;
+        Debug.Log(width);
+
+        //Canvasサイズ取得
+        float canvaswidth = CanvasPos.gameObject.GetComponent<RectTransform>().sizeDelta.x;
+        float canvasheight = CanvasPos.gameObject.GetComponent<RectTransform>().sizeDelta.y;
+
+        //目的地取得
+        Vector3 targetPos = new Vector3(CanvasPos.transform.position.x - text.rectTransform.sizeDelta.x, transform.position.y, transform.position.z);
+
+        //初期のスタート場所
+        text.transform.position = new Vector2(canvaswidth / 2 + width, 0);
+
         //移動するやつ
-        this.gameObject.transform.Translate(size, 0, 0);
+        while (true)
+        {
+            
+            //ベクトル取って
+            Vector3 ver = targetPos - transform.position;
+            //動かして
+            transform.position += ver * Time.deltaTime;
+            //距離はかって
+            float dis = Vector3.Distance(transform.position, targetPos);
+            //していないに行ったら終了
+            if (dis < 0.5f)
+            {
+                
+                transform.position = targetPos;
+                //適当な場所に戻す
+                text.transform.position = new Vector2(canvaswidth/2 + width, 0);
+                //break;    
+            }
+            yield return null;
+        }
     }
 }
